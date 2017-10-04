1c8027c5807c2ed63b04e1c0e56320bc9c7402c7
Josh Baker <joshbaker77@gmail.com>
2017-10-04 15:58:52 -0700

    updated denied message

--- controller/server/server.go
+++ controller/server/server.go
@@ -15,30 +15,23 @@ import (
 )
 
 // This phrase is copied nearly verbatim from Redis.
-// https://github.com/antirez/redis/blob/cf42c48adcea05c1bd4b939fcd36a01f23ec6303/src/networking.c
-var deniedMessage = []byte(strings.TrimSpace(`
-ACCESS DENIED
-Tile38 is running in protected mode because protected mode is enabled, no host
-address was specified, no authentication password is requested to clients. In
-this mode connections are only accepted from the loopback interface. If you
-want to connect from external computers to Tile38 you may adopt one of the
-following solutions:
-
-1) Disable protected mode by sending the command 'CONFIG SET protected-mode no'
-   from the loopback interface by connecting to Tile38 from the same host
-   the server is running, however MAKE SURE Tile38 is not publicly accessible
-   from internet if you do so. Use CONFIG REWRITE to make this change
-   permanent.
-2) Alternatively you can just disable the protected mode by editing the Tile38
-   configuration file, and setting the 'protected-mode' option to 'no', and
-   then restarting the server.
-3) If you started the server manually just for testing, restart it with the
-   '--protected-mode no' option.
-4) Use a host address or an authentication password.
-
-NOTE: You only need to do one of the above things in order for the server
-to start accepting connections from the outside.
-`) + "\r\n")
+var deniedMessage = []byte(strings.Replace(strings.TrimSpace(`
+-DENIED Tile38 is running in protected mode because protected mode is enabled,
+no bind address was specified, no authentication password is requested to
+clients. In this mode connections are only accepted from the loopback
+interface. If you want to connect from external computers to Tile38 you may
+adopt one of the following solutions: 1) Just disable protected mode sending
+the command 'CONFIG SET protected-mode no' from the loopback interface by
+connecting to Tile38 from the same host the server is running, however MAKE
+SURE Tile38 is not publicly accessible from internet if you do so. Use CONFIG
+REWRITE to make this change permanent. 2) Alternatively you can just disable
+the protected mode by editing the Tile38 configuration file, and setting the
+protected mode option to 'no', and then restarting the server. 3) If you
+started the server manually just for testing, restart it with the
+'--protected-mode no' option. 4) Setup a bind address or an authentication
+password. NOTE: You only need to do one of the above things in order for the
+server to start accepting connections from the outside.
+`), "\n", " ", -1) + "\r\n")
 
 // Conn represents a server connection.
 type Conn struct {
