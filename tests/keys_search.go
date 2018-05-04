a01281a52631c2670d198ca4ffaf8e0dfa1259e9
Alex Roitman <aroitman@housecanary.com>
2018-05-04 11:42:35 -0700

    Add tests for the new CIRCLE subcommand.

--- tests/keys_search.go
+++ tests/keys_search.go
@@ -4,6 +4,8 @@ import "testing"
 
 func subTestSearch(t *testing.T, mc *mockServer) {
 	runStep(t, mc, "KNN", keys_KNN_test)
+	runStep(t, mc, "WITHIN_CIRCLE", keys_WITHIN_CIRCLE_test)
+	runStep(t, mc, "INTERSECTS_CIRCLE", keys_INTERSECTS_CIRCLEtest)
 }
 
 func keys_KNN_test(mc *mockServer) error {
@@ -21,5 +23,38 @@ func keys_KNN_test(mc *mockServer) error {
 				"[1 [5 5] 2327116.1069888202] " +
 				"[4 [-5 5] 3227402.6159841116]" +
 				"]]"},
+	})
+}
+
+
+func keys_WITHIN_CIRCLE_test(mc *mockServer) error {
+	return mc.DoBatch([][]interface{}{
+		{"SET", "mykey", "1", "POINT", 37.7335, -122.4412}, {"OK"},
+		{"SET", "mykey", "2", "POINT", 37.7335, -122.44121}, {"OK"},
+		{"SET", "mykey", "3", "OBJECT", `{"type":"LineString","coordinates":[[-122.4408378,37.7341129],[-122.4408378,37.733]]}`}, {"OK"},
+		{"SET", "mykey", "4", "OBJECT", `{"type":"Polygon","coordinates":[[[-122.4408378,37.7341129],[-122.4408378,37.733],[-122.44,37.733],[-122.44,37.7341129],[-122.4408378,37.7341129]]]}`}, {"OK"},
+		{"SET", "mykey", "5", "OBJECT", `{"type":"MultiPolygon","coordinates":[[[[-122.4408378,37.7341129],[-122.4408378,37.733],[-122.44,37.733],[-122.44,37.7341129],[-122.4408378,37.7341129]]]]}`}, {"OK"},
+		{"SET", "mykey", "6", "POINT", -5, 5}, {"OK"},
+		{"SET", "mykey", "7", "POINT", 33, 21}, {"OK"},
+		{"WITHIN", "mykey", "IDS", "CIRCLE", 37.7335, -122.4412, 1000}, {
+			"[0 [1 2 3 4 5]]"},
+		{"WITHIN", "mykey", "IDS", "CIRCLE", 37.7335, -122.4412, 10}, {
+			"[0 [1 2]]"},
+	})
+}
+
+func keys_INTERSECTS_CIRCLEtest(mc *mockServer) error {
+	return mc.DoBatch([][]interface{}{
+		{"SET", "mykey", "1", "POINT", 37.7335, -122.4412}, {"OK"},
+		{"SET", "mykey", "2", "POINT", 37.7335, -122.44121}, {"OK"},
+		{"SET", "mykey", "3", "OBJECT", `{"type":"LineString","coordinates":[[-122.4408378,37.7341129],[-122.4408378,37.733]]}`}, {"OK"},
+		{"SET", "mykey", "4", "OBJECT", `{"type":"Polygon","coordinates":[[[-122.4408378,37.7341129],[-122.4408378,37.733],[-122.44,37.733],[-122.44,37.7341129],[-122.4408378,37.7341129]]]}`}, {"OK"},
+		{"SET", "mykey", "5", "OBJECT", `{"type":"MultiPolygon","coordinates":[[[[-122.4408378,37.7341129],[-122.4408378,37.733],[-122.44,37.733],[-122.44,37.7341129],[-122.4408378,37.7341129]]]]}`}, {"OK"},
+		{"SET", "mykey", "6", "POINT", -5, 5}, {"OK"},
+		{"SET", "mykey", "7", "POINT", 33, 21}, {"OK"},
+		{"INTERSECTS", "mykey", "IDS", "CIRCLE", 37.7335, -122.4412, 70}, {
+			"[0 [1 2 3 4 5]]"},
+		{"INTERSECTS", "mykey", "IDS", "CIRCLE", 37.7335, -122.4412, 10}, {
+			"[0 [1 2]]"},
 	})
 }
