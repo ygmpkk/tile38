632b0ebf63a21d1b243227651b1eb8deccb87af1
tidwall <joshbaker77@gmail.com>
2018-08-02 17:30:53 -0700

    Fix data race

--- pkg/controller/fence.go
+++ pkg/controller/fence.go
@@ -327,6 +327,8 @@ func fenceMatchObject(fence *liveFenceSwitches, obj geojson.Object) bool {
 }
 
 func fenceMatchRoam(c *Controller, fence *liveFenceSwitches, tkey, tid string, obj geojson.Object) (keys, ids []string, meterss []float64) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	col := c.getCol(fence.roam.key)
 	if col == nil {
 		return
